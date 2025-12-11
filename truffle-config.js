34644e145774f599e1b33216e72c0ec0f7717896
oussama-goussa <oussama.goussa0@gmail.com>
2025-12-11 11:48:38 +0100

    initial commit - complete Flutter DApp with Ethereum smart contract integration

--- truffle-config.js
+++ truffle-config.js
@@ -58,12 +58,18 @@ module.exports = {
    */
 
   networks: {
+    ganache: {
+      host: "127.0.0.1",     
+      port: 7545,           
+      network_id: "*",      
+     },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache, geth, or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
+
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
